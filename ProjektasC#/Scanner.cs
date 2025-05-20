24e0a6184b2dbfbee1a24d1a2824afba79aeed05
Domas <smikis423@gmail.com>
2025-05-21 00:19:20 +0300

    sukurta Scanner klasė, kiekvienas iš scanner objektų nuskaitys visus .txt failus, suindeksuos žodžius ir viską nusiūs Masteriui per Named pipes.

---
+++ ProjektasC#/Scanner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Scanner
+{
+    private string directoryPath;
+    private string pipeName;
+
+    public Scanner(string directoryPath, string pipeName)
+    {
+        this.directoryPath = directoryPath;
+        this.pipeName = pipeName;
+    }
+
+    private void readFiles()
+    {
+        using(NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", pipeName, PipeDirection.Out))
+        {
+            pipeClient.Connect();
+            foreach(string file in Directory.GetFiles(directoryPath,"*.txt"))
+            {
+                var wordCount = File.ReadAllLines(file)
+                    .SelectMany(line => line.Split(' '))
+                    .GroupBy(word => word)
+                    .ToDictionary(group => group.Key, group => group.Count());
+
+                using(StreamWriter writer = new StreamWriter(pipeClient))
+                {
+                    foreach (var entry in wordCount)
+                    {
+                        writer.WriteLine($"{Path.GetFileName(file)}:{entry.Key}:{entry.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
